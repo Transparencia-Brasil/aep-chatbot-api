1cf2677ebbeb7ad1d58d2fa30ed125ea36053051
Jefferson Neves <jefferson.rpn@gmail.com>
2022-01-06 16:38:34 -0300

    Abre rota de pedidos

--- src/routes.js
+++ src/routes.js
@@ -38,7 +38,7 @@ routes.get('/recursos/:id', checkToken, RecursoController.get);
 routes.get('/recursos/get/:ids', checkToken, RecursoController.getByIds);
 routes.get('/sics', checkToken, SicController.index);
 routes.get('/sics/pesquisar', checkToken, SicController.search);
-routes.get('/pedidos', checkToken, PedidoController.index);
+routes.get('/pedidos', PedidoController.index);
 routes.get('/pedidos/:id', checkToken, PedidoController.get);
 routes.post('/pedidos', checkToken, PedidoController.create);
 routes.put('/pedidos', checkToken, PedidoController.update);
